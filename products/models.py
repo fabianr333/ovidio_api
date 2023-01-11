6fca029bc712dc4c759cc96fc73482b3c10537b2
Fabian Mora <fabianr333@yahoo.com>
2023-01-11 18:24:32 -0500

    Models and migrations running

--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Brand(models.Model):
+    name = models.CharField(max_length=40)
+
+class Product(models.Model):
+    sku = models.CharField(max_length=8)
+    name = models.CharField(max_length=40)
+    price = models.FloatField()
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+
+
