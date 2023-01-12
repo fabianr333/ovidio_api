11eb7eaf96888a948a22f1aecc610af426418bd2
Fabian Mora <fabianr333@yahoo.com>
2023-01-11 19:51:59 -0500

    views implemented

--- products/views.py
+++ products/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from .models import Product, Brand
+from rest_framework import viewsets
+from .serializers import ProductSerializer, BrandSerializer
 
-# Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows products to be viewed or edited.
+    """
+    queryset = Product.objects.all().order_by('-id')
+    serializer_class = ProductSerializer
+    permission_classes = []
+
+class BrandViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allow brands to be viewed or edited
+    """
+    queryset = Brand.objects.all().order_by('-id')
+    serializer_class = BrandSerializer
+    permission_classes = []
