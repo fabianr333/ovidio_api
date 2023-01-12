ba7d8ee1dfa32025c812a87c20e1bcd8d0cea5b8
Fabian Mora <fabianr333@yahoo.com>
2023-01-11 19:50:48 -0500

    model updated to str representation

--- products/models.py
+++ products/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Brand(models.Model):
     name = models.CharField(max_length=40)
 
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     sku = models.CharField(max_length=8)
     name = models.CharField(max_length=40)
