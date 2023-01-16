82bddcb0dcdf193cbc425534332c13f74605f02d
Fabian Mora <fabianr333@yahoo.com>
2023-01-16 17:57:04 -0500

    protecting routes

--- products/views.py
+++ products/views.py
@@ -1,5 +1,9 @@
-from .models import Product, Brand
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Product, Brand
 from .serializers import ProductSerializer, BrandSerializer
 
 class ProductViewSet(viewsets.ModelViewSet):
@@ -8,7 +12,7 @@ class ProductViewSet(viewsets.ModelViewSet):
     """
     queryset = Product.objects.all().order_by('-id')
     serializer_class = ProductSerializer
-    permission_classes = []
+    permission_classes = [IsAuthenticated]
 
 class BrandViewSet(viewsets.ModelViewSet):
     """
@@ -16,4 +20,15 @@ class BrandViewSet(viewsets.ModelViewSet):
     """
     queryset = Brand.objects.all()
     serializer_class = BrandSerializer
-    permission_classes = []
+    permission_classes = [IsAuthenticated]
+
+
+@api_view(['GET'])
+def product_list(request):
+    """
+    List all the products for non protected views
+    """
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
