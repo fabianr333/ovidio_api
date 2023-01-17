a31fa551e41e5b4feada3fa6173f30dfb64a41a3
Fabian Mora <fabianr333@yahoo.com>
2023-01-16 19:44:03 -0500

    overriding update method on ProductViewSet to mail admins

--- products/views.py
+++ products/views.py
@@ -2,6 +2,9 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.core.mail import send_mail
+from django.conf import settings
+from django.contrib.auth.models import User
 
 from .models import Product, Brand
 from .serializers import ProductSerializer, BrandSerializer
@@ -13,6 +16,21 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all().order_by('-id')
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticated]
+
+    def update(self, request, *args, **kwargs):
+        super().update(request, *args, **kwargs)
+        instance = self.get_object()
+
+        # let's notify other admins
+        admins = User.objects.all()
+        send_mail(
+            subject='A product has been updated',
+            message=f'{instance.name} has changed to {instance.name}, {instance.brand}, {instance.price}',
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[u.email for u in admins]
+        )
+
+        return Response(ProductSerializer(instance).data)
 
 class BrandViewSet(viewsets.ModelViewSet):
     """
