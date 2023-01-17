57be725a5b06b371153004f8060b67946c80f499
Fabian Mora <fabianr333@yahoo.com>
2023-01-16 21:26:10 -0500

    moving the secret key to .env file

--- api/settings.py
+++ api/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d#-f2&teaw8_k*%nud7^suk(l-c1h6&e&-hqr#epaw6gqkjg*('
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
