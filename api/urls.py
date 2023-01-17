3a935d2253f92ba0d1c53818b81332032751a28f
Fabian Mora <fabianr333@yahoo.com>
2023-01-16 20:37:41 -0500

    Fixing the right api prefix

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('', include('products.urls')),
+    path('api/', include('products.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),    
 ]
